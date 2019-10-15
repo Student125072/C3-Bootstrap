b2c9bf763473c3dca90d3b5bc92fb288115584fc
Kevin <koster.2001@gmail.com>
2019-10-15 13:31:10 +0200

    rework, opdracht was verkeerd

---
+++ eindopdracht/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- bootstrap import -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <!-- css links -->
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans"/>
+
+    <!-- titel -->
+    <title>Nieuwscollectie.nl</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+    <nav class="nav navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="glyphicon glyphicon-th-list listicon"></span>
+          </button>
+          <span class="glyphicon glyphicon-globe logo"></span>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-left">
+            <li><a href="#">NIEUWS</a></li>
+            <li><a href="#">BRONNEN</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron text-center">
+      <div class="container">
+      <h1>Nieuwscollectie</h1>
+      <p>Al jouw nieuws op één plek.</p>
+    </div>
+    </div>
+
+
+  </body>
+</html>
