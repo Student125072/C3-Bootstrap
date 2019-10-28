73b1234bab9a86b2c3e467332b1c94ee4528ac28
Kevin <koster.2001@gmail.com>
2019-10-28 10:20:23 +0100

    hoi

--- eindopdracht/nieuws.php
+++ eindopdracht/nieuws.php
@@ -41,13 +41,13 @@
         <div class="col-sm-2">
           <ul class="list-group">
             <li class="list-group-item li-header">Categorieën</li>
-            <a href="#" class="list-group-item list-group-item-action li-link active">
+            <a href="#" class="list-group-item list-group-item-action li-link globaal active">
               Globaal<span class="badge badge-primary badge-pill">2</span>
             </a> <!-- a -->
-            <a href="#" class="list-group-item list-group-item-action li-link">
+            <a href="#" class="list-group-item list-group-item-action li-link regionaal">
               Regionaal<span class="badge badge-primary badge-pill">1</span>
             </a> <!-- a -->
-            <a href="#" class="list-group-item list-group-item-action li-link">
+            <a href="#" class="list-group-item list-group-item-action li-link weer">
               Weer<span class="badge badge-primary badge-pill">1</span>
             </a> <!-- a -->
           </ul>
@@ -58,12 +58,70 @@
 
       <div class="col-sm-10">
           <div class="well">
-            
-          </div>
+            <div class="globcollapse">
+              <h1 class="text-center">Globaal</h1>
+              <div class="well news">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <h2>Lorem ipsum</h2>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  </div> <!-- col -->
+                  <div class="col-sm-6">
+                    <img src="img/carousel1.jpg">
+                  </div>
+                </div> <!-- row -->
+              </div>
+            </div>
+
+
+            <div class="regcollapse">
+
+            </div>
+
+
+
+          </div> <!-- well -->
       </div>
 
 
+      <script>
+        $(document).ready(function(){
 
+          $(".globaal").click(function(){
+            $(".regcollapse").collapse('hide');
+            $(".weercollapse").collapse('hide');
+            $(".globcollapse").collapse('show');
+            $(".globaal").addClass('active');
+            $(".weer").removeClass('active');
+            $(".regionaal").removeClass('active');
+          });
+
+          $(".regionaal").click(function(){
+            $(".globcollapse").collapse('hide');
+            $(".weercollapse").collapse('hide');
+            $(".regcollapse").collapse('show');
+            $(".regionaal").addClass('active');
+            $(".globaal").removeClass('active');
+            $(".weer").removeClass('active');
+          });
+
+          $(".weer").click(function(){
+            $(".globcollapse").collapse('hide');
+            $(".regcollapse").collapse('hide');
+            $(".weercollapse").collapse('show');
+            $(".weer").addClass('active');
+            $(".globaal").removeClass('active');
+            $(".regionaal").removeClass('active');
+          });
+
+
+        })
+      </script>
 
 
       </div> <!-- row -->
